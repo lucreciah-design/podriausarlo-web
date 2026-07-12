f0fb973a30dbfd0c81d92a5a2f106b9b5e1fff20
lucreciah-design <lucreciahandula@gmail.com>
2026-07-12 01:25:04 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyADNiE9L1_LnGYTZgNqAQT-p6kjhq9mNpQ",
-  authDomain: "podriausarlo-308a1.firebaseapp.com",
-  projectId: "podriausarlo-308a1",
-  storageBucket: "podriausarlo-308a1.firebasestorage.app",
-  messagingSenderId: "541732226737",
-  appId: "1:541732226737:web:ee8225b50317e577ccc490"
-};
